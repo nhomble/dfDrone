c3cf635f750c28889d7d2a833b849277469ceea2
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-07 12:24:59 -0500

    A dumb beginning at my attempt to log what is going on to ROS. I will need to decide how I want to log my python scripts

--- launch_dfDrone.cpp
+++ launch_dfDrone.cpp
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <ros/console.h>
+
 int main(){
+	ROS_DEBUG("Starting dfDrone");
 	system("./dfDrone.py");
+	ROS_DEBUG("Ending program... should I have looped?");
 	return 0;	
 }
