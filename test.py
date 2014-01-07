e3b6e02c02fbff60d47230a0b27e2505d77c149e
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-07 11:04:52 -0500

    The test script is complete. Simply, I just ask the detector if it thinkgs the image has an ARDrone or not.

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python2
 
-import 
+import sys
+
+import SimpleCV
+
+import droneDetect
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv
+	if len(argv) != 3:
+		print(str(argv[0]) + " positive/negative imgPath")
+	isPositive = False;
+	if argv[1] == "positive":
+		isPositive = True;
+	img = SimpleCV.Image(argv[2])
+	
+	detector = droneDetect.Detector(img)
+	if isPositive == detector.hasDrone(img):
+		pass
+	else:
+		print(argv[2])
+
+if __name__ == "__main__":
+	sys.exit(main())
