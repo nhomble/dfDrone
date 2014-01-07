710f032c32724075f5f9c53df3e2301b706cf255
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-07 12:25:48 -0500

    More organizational things

--- droneDetect.py
+++ droneDetect.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2
 
+import time
+
+import SimpleCV
+
 class Detector():
 	def __init__(self, img):
 		self.prevImg = img
@@ -8,6 +12,7 @@ class Detector():
 		self.avgCount = 1
 
 		self.lastSeenImg = None
+		self.lastTime = None
 	# give the detector another image that is 
 	# hopefully representative of the environment
 	# and then calculate the average color in the image
@@ -16,17 +21,21 @@ class Detector():
 		self.avgColor = ((self.avgCount * self.avgColor) + newVal)/(self.avgCount + 1)
 		self.avgCount += 1
 
-	def process(self, img):
-		if(hasDrone(img)):
+	def process(self, img, depth):
+		if(hasDrone(img, depth)):
 			if(self.lastSeenImg is None):
 				self.lastSeenImg = img
+			if(self.lastTime is None):
+				self.lastTime = time.gmtime()
+		# eventually I should return a tuple of the new delX, delY	
+			return True, None, None
+		return False, None, None
 			
 
 	# the whole point of this class is to detect whether
 	# a drone is in the image
-	def hasDrone(self, img):
-		return False
-
+	def hasDrone(self, img, depth):
+		return false
 	'''
 	the following are wrappers of simplecv feature detection functions
 	here I wanted to draw the features when found for debugging purposes
