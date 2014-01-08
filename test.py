92329cc0d4886f19887cb74d9d289d9c5f808322
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-08 12:19:01 -0500

    fixed paths to image files

--- test.py
+++ test.py
@@ -14,13 +14,13 @@ def main(argv=None):
 	isPositive = False;
 	if argv[1] == "positive":
 		isPositive = True;
-	img = SimpleCV.Image(argv[2])
+	img = SimpleCV.Image("./" + str(argv[2]))
 	
 	detector = droneDetect.Detector(img)
 	if isPositive == detector.hasDrone(img, None):
 		pass
 	else:
-		print(argv[2])
+		print("./" + argv[2])
 
 if __name__ == "__main__":
 	sys.exit(main())
