4240b3b5f94c5bc98791985b2c2ebf04e1c02e8e
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-08 12:17:47 -0500

    added code so that it is clear how I will use the kinect's depth sensor

--- dfDrone.py
+++ dfDrone.py
@@ -8,14 +8,23 @@ import freenect
 import droneDetect
 import twist
 
+# TODO should use a better command line parser 
 def main(argv=None):
 	if argv is None:
 		argv = sys.argv
-	
+
+	# by default use a regular camera
 	cam = SimpleCV.Camera()
+	useKinect = False
+
+	# but try to get the kinect
+	if len(argv) > 1 and argv[1] is "kinect":
+		cam = SimpleCV.Kinect()
+		useKinect = True
+
 	disp = SimpleCV.Display()
 
-	detector = droneDetect.Detector(cam.getImage())
+	detector = droneDetect.Detector(cam.getImage(), useKinect)
 	control = twist.Control() 
 
 	# use the display for debugging purposes
@@ -23,6 +32,10 @@ def main(argv=None):
 	while disp.isNotDone():
 		img = cam.getImage()
 		depth = None
+
+		if useKinect is True:
+			depth = cam.getDepth()
+
 		# ask our detector for new coordinates 
 		valid, delX, delY = detector.process(img, depth)
 		if valid is True:
