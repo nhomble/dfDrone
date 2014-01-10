a81c126c509f336ab0e7d9265bb8e067db2cadb0
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-10 13:10:13 -0500

    creating skeleton for sending twist messages

--- libtwist.cpp
+++ libtwist.cpp
@@ -3,4 +3,25 @@
 
 #include "ros/ros.h"
 
+class Twist {
+	ros::NodeHandle n;
+	ros::Publisher pub;
+	
+	public:
+		void sendMessage(int x, int y){
 
+		} 
+};
+
+Twist::Twist(){
+	pub = n.advertise<std_msgs::cmd_vel>("twist", 100);
+}
+
+extern "C" {
+	Twist* Twist_new(){
+		return new Twist();
+	}
+	void Twist_sendMessage(Twist* twist, int x, int y){
+		twist->sendMessage(x, y);
+	}
+}
