4b6bfa8ea023ba235043471cfbc0f910f9b0dc38
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-10 12:24:50 -0500

    trying to add parallelism to the twist calls with python threads

--- twist.py
+++ twist.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 
-import sys
-
 from ctypes import cdll
+import sys
+import threading
 
 class Control(object):
 	def __init__(self):
@@ -13,6 +13,25 @@ class Control(object):
 		except IOError:
 			print("where is libtwist?, make sure the file is compiled and in the same directory")
 		self.lib = cdll.LoadLibrary('./libtwist')
+		self.velocityQueue = []
+		thread = threading.Thread(target=popVelocity, args=(self, ))	
+		thread.start()
+		thread.join()
 	
-	def addToCoordinateQueue(self, delX, delY):
+	def addToVelocityQueue(self, delX, delY):
+		thread = threading.Thread(target=addToVelocityQueueThreaded, args=(self, (delX, delY)))
+		thread.start()
+		thread.join()
+
+def addToVelocityQueueThreaded(control, velocity):
+	control.velocityQueue.append(velocity)
+
+def popVelocity(control):
+	while True:
+		thread = threading.Thread(target=popVelocityThreaded, args=(control, ))
+		thread.start()
+		thread.join()
+
+def popVelocityThreaded(control):
+	if len(control.velocityQueue) == 0:
 		pass
