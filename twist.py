cc9db9e7d0e906193c976261bcda7c5dd704c58f
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-10 13:08:13 -0500

    added comments

--- twist.py
+++ twist.py
@@ -4,6 +4,12 @@ from ctypes import cdll
 import sys
 import threading
 
+'''
+need to review python threading!
+
+with threads I hope to parallelize the queue of velocity vectors and
+the processing of these vectors with twist messages to ROS (via cytpes)
+'''
 class Control(object):
 	def __init__(self):
 			
@@ -13,11 +19,16 @@ class Control(object):
 		except IOError:
 			print("where is libtwist?, make sure the file is compiled and in the same directory")
 		self.lib = cdll.LoadLibrary('./libtwist')
+		self.twist = self.lib.Twist_new()
+
 		self.velocityQueue = []
+
+		# start thread to spawn more threads to pop velocity vectors
 		thread = threading.Thread(target=popVelocity, args=(self, ))	
 		thread.start()
 		thread.join()
 	
+	# spawn thread to add velocity vector to queue
 	def addToVelocityQueue(self, delX, delY):
 		thread = threading.Thread(target=addToVelocityQueueThreaded, args=(self, (delX, delY)))
 		thread.start()
@@ -35,3 +46,7 @@ def popVelocity(control):
 def popVelocityThreaded(control):
 	if len(control.velocityQueue) == 0:
 		pass
+
+	# send twist message via ctypes
+	velTuple = control.velocityQueue.pop()
+	control.lib.twist_sendMessage(control.twist, velTuple[0], velTuple[1]);
