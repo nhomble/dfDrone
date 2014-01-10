d5470457d2ae12a11c2959b5ed20b81651398b00
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-10 14:02:09 -0500

    cannot make ROS calls since I have not create a ROS environment yet

--- launch_dfDrone.cpp
+++ launch_dfDrone.cpp
@@ -1,11 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sstream>
 
-#include <ros/console.h>
+#include "ros/ros.h"
 
-int main(){
-	ROS_DEBUG("Starting dfDrone");
-	system("./dfDrone.py kinect");
-	ROS_DEBUG("Ending program... should I have looped?");
+int main(int argc, char *argv[]){
+//	ros::init(argc, argv, "dfDrone");
+
+//	while(ros::ok()){
+//		ROS_DEBUG("Starting dfDrone");
+		system("./dfDrone.py kinect");
+//		ROS_DEBUG("Ending program... going to loop");
+//	}
 	return 0;	
 }
