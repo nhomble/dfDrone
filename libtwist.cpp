868be458b8827bf97bf8aa59e14b8b1f2d903ba3
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-10 14:03:05 -0500

    cleaning up the skeleton, again I cannot make ROS calls until I make the workspace

--- libtwist.cpp
+++ libtwist.cpp
@@ -6,15 +6,17 @@
 class Twist {
 	ros::NodeHandle n;
 	ros::Publisher pub;
-	
+
 	public:
+		Twist ();
+		// need to create twist message and send via ROS
 		void sendMessage(int x, int y){
 
 		} 
-};
+} twist;
 
+// need to initialize the publisher
 Twist::Twist(){
-	pub = n.advertise<std_msgs::cmd_vel>("twist", 100);
 }
 
 extern "C" {
