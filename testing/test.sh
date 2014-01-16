c6f3b14098b752fc9434c18e8b04d0cda7664a0b
SEIL-user <seil-user@EAB0200A.(none)>
2014-01-16 17:02:45 -0500

    more cleaning of the repo, I have moved files around

--- testing/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PWD=$(pwd)
-for f in $(find ./positive -type f); do
-	python2 ../test.py positive $PWD/$f
-done
-
-for f in $(find ./negative -type f); do
-	python2 ../test.py negative $PWD/$f
-done
