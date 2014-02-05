786462198ad856cba1e534cd48489159448e0609
Nicolas Homble <nhomble@terpmail.umd.edu>
2014-02-05 02:46:36 -0500

    view streams live - eventually I should try to see draw the velocity vectors

---
+++ scripts/colorLiveTest.py
@@ -0,0 +1,25 @@
+#!/bin/env python2
+
+import sys
+import time
+import SimpleCV
+import droneDetect
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv
+	cam = SimpleCV.Camera()
+	disp = SimpleCV.Display()
+
+	detector = droneDetect.Detector(cam.getImage(), False, True)
+
+	while disp.isNotDone():
+		img = cam.getImage()
+
+		hasDrone, x, y, z, t = detector.process(img, None)
+		#if hasDrone is True:
+			#img.drawText(text)
+			#img.show()
+
+if __name__ == "__main__":
+	sys.exit(main())
