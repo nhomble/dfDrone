f4121b57fe44b1d64fbf4a05a8aa4fec2718a489
tb04-l <turtlebot@tb04-l.(none)>
2014-03-28 19:02:41 -0400

    depracated - I will probably remove soon

--- scripts/velocityDrone.py
+++ scripts/velocityDrone.py
@@ -5,30 +5,19 @@ from std_msgs.msg import Float64MultiArray
 class DFDVelocity():
 	def __init__(self, flag, delx, dely, z, delay):
 		self.isPresent = flag
-		self.delX = None
-		self.delY = None
-		self.z = None
-		self.delay = None
+		self.delX = 0.0
+		self.delY = 0.0
+		self.z = 0.0
+		self.delay = 0.0
 		if self.isPresent is True:
-			self.delX = delx
-			self.delY = dely
-			self.z = z
-			sel.delay = delay
+			self.delX = float(delx)
+			self.delY = float(dely)
+			self.z = float(z)
+			sel.delay = float(delay)
 
 	def ros(self):
 		message = Float64MultiArray()
-		message.layout.dim[0].label = "velocity"
-		message.layout.dim[0].size = 5
-		message.layout.dim[0].stride = 1
-
-		message.layout.dim[1].label = "junk"
-		message.layout.dim[1].size = 0
-		message.layout.dim[1].stride = 0
-
-		message.layout.dim[2].label = "junk"
-		message.layout.dim[2].size = 0
-		message.layout.dim[3].size = 0
-
+		
 		boolean = 0.0
 		if self.isPresent is True:
 			boolean = 1.0
